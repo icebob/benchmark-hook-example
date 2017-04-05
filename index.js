9346d7dd984e96b5e210c7f9f961489c2d9caf2f
Icebob <mereg.norbert@gmail.com>
2017-04-05 21:56:12 +0200

    add more tests

--- index.js
+++ index.js
@@ -3,21 +3,38 @@
 let Benchmarkify = require("benchmarkify");
 Benchmarkify.printHeader("Simple example");
 
-let bench = new Benchmarkify({ async: false, name: "String concatenate", resultFile: "./bench-results/simple.json"});
+let bench1 = new Benchmarkify({ async: false, name: "String concatenate"});
 
 const ITERATION = 1000;
-bench.add("Concat with '+'", () => {
+bench1.add("Concat with '+'", () => {
 	let s = "";
 	for(let i = 0; i < ITERATION; i++)
 		s += "test" + i;
 	return s;
 });
 
-bench.add("Concat with array & join", () => {
+bench1.add("Concat with array & join", () => {
 	let s = [];
 	for(let i = 0; i < ITERATION; i++)
 		s.push("test" + i);
 	return s.join();
 });
 
-bench.run();
+let bench2 = new Benchmarkify({ async: false, name: "Increment integer"});
+
+let i1 = 0;
+bench2.add("Increment with ++", () => {
+	i1++;
+});
+
+let i2 = 0;
+bench2.add("Increment with +=", () => {
+	i2 += 1;
+});
+
+let i3 = 0;
+bench2.add("Increment with = i + 1", () => {
+	i3 = i3 + 1;
+});
+
+module.exports = Benchmarkify.run([bench1, bench2])
