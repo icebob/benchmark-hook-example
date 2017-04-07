1b01bbbdc185166b8e07ea68285ea1c7a4911eb8
Icebob <mereg.norbert@gmail.com>
2017-04-07 15:33:30 +0200

    add second test

--- benchmark/test1.js
+++ benchmark/test1.js
@@ -3,17 +3,34 @@
 let Benchmarkify = require("benchmarkify");
 let benchmark = new Benchmarkify("Simple example").printHeader();
 
-let bench = benchmark.createSuite({ name: "String concatenate" });
+let bench1 = benchmark.createSuite({ name: "String concatenate" });
 
 let main = require("../index");
 
 const ITERATION = 1000;
-bench.add("Concat with '+'", () => {
+bench1.add("Concat with '+'", () => {
 	return main.concatV1(ITERATION);
 });
 
-bench.add("Concat with array & join", () => {
+bench1.ref("Concat with array & join", () => {
 	return main.concatV2(ITERATION);
 });
 
-module.exports = benchmark.run([bench]);
+let bench2 = benchmark.createSuite({ name: "Increment integer"});
+
+let i1 = 0;
+bench2.add("Increment with ++", () => {
+	i1++;
+});
+
+let i2 = 0;
+bench2.skip("Increment with +=", () => {
+	i2 += 1;
+});
+
+let i3 = 0;
+bench2.add("Increment with = i + 1", () => {
+	i3 = i3 + 1;
+});
+
+module.exports = benchmark.run([bench1, bench2]);
